7ca1b786f0b1451382a09e6a9418ff3cd95c264f
Theo <theosech@mit.edu>
2020-01-29 14:39:36 -0800

    web platform additions

--- arc-data/apps/js/testing_interface.js
+++ arc-data/apps/js/testing_interface.js
@@ -5,12 +5,12 @@ var CURRENT_OUTPUT_GRID = new Grid(3, 3);
 var TEST_PAIRS = new Array();
 var CURRENT_TEST_PAIR_INDEX = 0;
 var COPY_PASTE_DATA = new Array();
+var TASK_FROM_LIST_COUNT = 0
 
 // Cosmetic.
 var EDITION_GRID_HEIGHT = 500;
 var EDITION_GRID_WIDTH = 500;
 var MAX_CELL_SIZE = 100;
-
 
 function resetTask() {
     CURRENT_INPUT_GRID = new Grid(3, 3);
@@ -158,13 +158,83 @@ function loadTaskFromFile(e) {
         loadJSONTask(train, test);
     };
     reader.readAsText(file);
+    document.getElementById('taskName').innerHTML = file.name;
+    document.getElementById('random_task').innerHTML = '';
+    document.getElementById('task_from_list').innerHTML = '';
 }
 
-function randomTask() {
-    // document.getElementById('task_demo_header').removeChild(document.getElementById('name').node);
+function taskFromList() {
+    console.log(TASK_FROM_LIST_COUNT)
+    var taskList = 
+['6d0aefbc.json',
+'97999447.json',
+'ce4f8723.json',
+'c9e6f938.json',
+'72ca375d.json',
+'0520fde7.json',
+'5521c0d9.json',
+'007bbfb7.json',
+'c59eb873.json',
+'fcb5c309.json',
+'f25fbde4.json',
+'67e8384a.json',
+'50cb2852.json',
+'6150a2bd.json',
+'68b16354.json',
+'8f2ea7aa.json',
+'6fa7a44f.json',
+'3af2c5a8.json',
+'62c24649.json',
+'4347f46a.json',
+'3c9b0459.json',
+'4c4377d9.json',
+'67a3c6ac.json',
+'a416b8f3.json',
+'1cf80156.json',
+'bb43febb.json',
+'9172f3a0.json',
+'8be77c9e.json',
+]
+
+var programList = 
+[
+'(lambda (solvec9e6f938 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solve97999447 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (_solvece4f8723 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solvec9e6f938 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (_solve72ca375d $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solve0520fde7 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (_solve5521c0d9 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solve007bbfb7 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solvef25fbde4 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solvefcb5c309 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000',
+'(lambda (solvef25fbde4 $0)) ; log prior = -6.591674 ; log likelihood = 0.000000', 
+'(lambda (concatNAndReflect (solvec9e6f938 $0) true down)) ; log prior = -12.660099 ; log likelihood = 0.000000',
+'(lambda (solve50cb2852 $0 teal)) ; log prior = -9.230731 ; log likelihood = 0.000000',
+'(lambda (reflect (reflect $0 false) true)) ; log prior = -12.660099 ; log likelihood = 0.000000',
+'(lambda (reflect $0 true)) ; log prior = -7.977968 ; log likelihood = 0.000000',
+'(lambda (solve007bbfb7 (blocksToMinGrid (findBlocksByCorner $0)))) ; log prior = -12.190096 ; log likelihood = 0.000000',
+'(lambda (concatNAndReflect $0 true down)) ; log prior = -9.364262 ; log likelihood = 0.000000',
+'(lambda (concatNAndReflect (solvec9e6f938 $0) true down)) ; log prior = -12.660099 ; log likelihood = 0.000000',
+'(lambda (concatNAndReflect (solvec9e6f938 $0) true down)) ; log prior = -12.660099 ; log likelihood = 0.000000',
+'(lambda (solve50cb2852 $0 black)) ; log prior = -9.230731 ; log likelihood = 0.000000',
+'(lambda (reflect (reflect $0 false) true)) ; log prior = -12.660099 ; log likelihood = 0.000000',
+'(lambda (concatNAndReflect $0 true up)) ; log prior = -9.364262 ; log likelihood = 0.000000',
+'(lambda (reflect $0 false)) ; log prior = -7.977968 ; log likelihood = 0.000000',
+'(lambda (duplicateN $0 left 1)) ; log prior = -10.922407 ; log likelihood = 0.000000',
+'(lambda (blocksToMinGrid (findBlocksByCorner $0))) ; log prior = -8.894259 ; log likelihood = 0.000000',
+'(lambda (solve50cb2852 $0 red)) ; log prior = -9.230731 ; log likelihood = 0.000000',
+'(lambda (grow $0 3)) ; log prior = -9.536113 ; log likelihood = 0.000000',
+'(lambda (concatNAndReflect $0 true down)) ; log prior = -9.364262 ; log likelihood = 0.000000',
+]
+
+    document.getElementById('task_from_list').innerHTML = TASK_FROM_LIST_COUNT + ' ' + taskList[TASK_FROM_LIST_COUNT];
+    document.getElementById('program_found').innerHTML = programList[TASK_FROM_LIST_COUNT]
+    document.getElementById('random_task').innerHTML = '';
     var subset = "training";
     $.getJSON("https://api.github.com/repos/fchollet/ARC/contents/data/" + subset, function(tasks) {
-      var task = tasks[Math.floor(Math.random() * tasks.length)];
+      var task = tasks.find(task => task.name == taskList[TASK_FROM_LIST_COUNT])
+      TASK_FROM_LIST_COUNT = (TASK_FROM_LIST_COUNT + 1) % taskList.length
       $.getJSON(task["download_url"], function(json) {
           try {
               train = json['train'];
@@ -182,6 +252,36 @@ function randomTask() {
       });
         document.getElementById('taskName').innerHTML = task['name'];
 
+    })
+    .error(function(){
+      errorMsg('Error loading task list');
+    });
+}
+
+function randomTask() {
+    TASK_FROM_LIST_COUNT = 0
+    var subset = "training";
+    $.getJSON("https://api.github.com/repos/fchollet/ARC/contents/data/" + subset, function(tasks) {
+      var task = tasks.find(task => task.name == 'fcb5c309.json')
+      var task = tasks[Math.floor(Math.random() * tasks.length)];
+      $.getJSON(task["download_url"], function(json) {
+          try {
+              train = json['train'];
+              test = json['test'];
+          } catch (e) {
+              errorMsg('Bad file format');
+              return;
+          }
+          loadJSONTask(train, test);
+          $('#load_task_file_input')[0].value = "";
+          infoMsg("Loaded task training/" + task["name"]);
+      })
+      .error(function(){
+        errorMsg('Error loading task');
+      });
+        document.getElementById('taskName').innerHTML = task['name'];
+        document.getElementById('random_task').innerHTML = task['name'];
+        document.getElementById('task_from_list').innerHTML = '';
     })
     .error(function(){
       errorMsg('Error loading task list');
