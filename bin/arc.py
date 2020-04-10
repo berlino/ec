17bbf76313634e00afae22798b004e88f1d761ab
Theo <theosech@mit.edu>
2020-04-10 12:17:47 -0400

    unconditional bigram model changes

--- bin/arc.py
+++ bin/arc.py
@@ -6,6 +6,7 @@ except ModuleNotFoundError:
 
 from dreamcoder.domains.arc.main import list_options, main
 from dreamcoder.dreamcoder import commandlineArguments
+from dreamcoder.recognition import DummyFeatureExtractor
 from dreamcoder.utilities import numberOfCPUs
 
 
@@ -13,9 +14,9 @@ if __name__ == '__main__':
     # print(retrieveARCJSONTasks('/Users/theo/Development/program_induction/ARC/data/training/007bbfb7.json')[1])
 
     args = commandlineArguments(
-        enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600,
+        enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600, featureExtractor=DummyFeatureExtractor,
         a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
-        helmholtzRatio=0.5, structurePenalty=1.,
+        helmholtzRatio=0, structurePenalty=1.,
         CPUs=numberOfCPUs(),
         extras=list_options)
     main(args)
