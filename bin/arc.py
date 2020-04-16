3333ab767bbe5c04c4d2d642a297b13ec925b571
Theo Sechopoulos <theosech@mit.edu>
2020-04-16 11:01:06 -0400

    added dummy feature extractor

--- bin/arc.py
+++ bin/arc.py
@@ -5,6 +5,7 @@ except ModuleNotFoundError:
     import bin.binutil  # alt import if called as module
 
 from dreamcoder.domains.arc.main import list_options, main
+from dreamcoder.recognition import DummyFeatureExtractor
 from dreamcoder.dreamcoder import commandlineArguments
 from dreamcoder.utilities import numberOfCPUs
 
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     # print(retrieveARCJSONTasks('/Users/theo/Development/program_induction/ARC/data/training/007bbfb7.json')[1])
 
     args = commandlineArguments(
-        enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600,
+        enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600, featureExtractor=DummyFeatureExtractor,
         a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
         helmholtzRatio=0.5, structurePenalty=1.,
         CPUs=numberOfCPUs(),
