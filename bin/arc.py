ee46e6f3e20d7dfbf4c25b6da9f062284d76a8c8
Theo Sechopoulos <theosech@mit.edu>
2020-05-01 14:24:44 -0400

    pseudo1 and DummyFeatureExtractor

--- bin/arc.py
+++ bin/arc.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     # print(retrieveARCJSONTasks('/Users/theo/Development/program_induction/ARC/data/training/007bbfb7.json')[1])
 
     args = commandlineArguments(
-        enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600, featureExtractor=ArcCNN,
-        a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
+        enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600, featureExtractor=DummyFeatureExtractor,
+        a=3, maximumFrontier=10, topK=2, pseudoCounts=1.0,
         helmholtzRatio=0, structurePenalty=1.,
         CPUs=numberOfCPUs(),
         extras=list_options)
