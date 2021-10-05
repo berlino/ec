355f59ec0c361fd3f88ae4c0b1dcabbb60057955
Theo Sechopoulos <theosech@mit.edu>
2021-10-05 12:57:32 -0400

    renamed directory with neural sequence decoder model from larc to neural_seq

---
+++ bin/run_neural_seq.py
@@ -0,0 +1,13 @@
+import binutil  # required to import from dreamcoder modules
+except ModuleNotFoundError:
+    import bin.binutil  # alt import if called as module
+
+from neural_seq.model import main
+from neural_seq.utils import commandLineArgs
+from neural_seq import decoderUtils
+import os
+
+if __name__ == '__main__':
+    print("current PID:{}".format(os.getpid()))
+    args = commandLineArgs()
+    main(args)
