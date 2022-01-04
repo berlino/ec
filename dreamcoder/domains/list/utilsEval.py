2b113cf8b0d72c77695673df354b5c04856e6bd5
Theo <theosech@mit.edu>
2022-01-04 12:12:16 -0800

    new plotting functions

--- dreamcoder/domains/list/utilsEval.py
+++ dreamcoder/domains/list/utilsEval.py
@@ -1,9 +1,17 @@
 import datetime
 import dill
+import pickle
 
-from dreamcoder.domains.list.utilsPlotting import plotFrontiers
+# from dreamcoder.domains.list.utilsPlotting import plotFrontiers
 from dreamcoder.enumeration import multicoreEnumeration
 from dreamcoder.grammar import Grammar
+
+def loadEnumerationResults(filename):
+    try:
+        frontiers, times, _ = pickle.load(open("enumerationResults/{}".format(filename), "rb"))
+    except ValueError:
+        frontiers, times = pickle.load(open("enumerationResults/{}".format(filename), "rb"))
+    return frontiers, times
 
 def getRecognizerTaskGrammars(trainedRecognizer, tasks):
     grammars = {task: trainedRecognizer.grammarOfTask(task)
@@ -44,7 +52,7 @@ def enumerateFromGrammars(args, tasks, allGrammars, modelNames, save, plotName):
          numTasksSolved = len([f.task for f in nonEmptyFrontiers if f.task.check(f.topK(1).entries[0].program, timeout=1.0, leaveHoldout=False)])
          print("Enumerating from {} grammars for {} seconds: {} / {} actually true for holdout example".format(modelName, enumerationTimeout, numTasksSolved, len(nonEmptyFrontiers)))
     
-    plotFrontiers(modelNames, enumerationResults=enumerationResults, save=True, plotName=plotName)
+    # plotFrontiers(modelNames, enumerationResults=enumerationResults, save=True, plotName=plotName)
     return
 
 def enumerationProxy(task2FittedGrammars, tasks, modelNames, verbose=False):
@@ -57,7 +65,7 @@ def enumerationProxy(task2FittedGrammars, tasks, modelNames, verbose=False):
         modelNames (list(str)): List of model names corresponding to task2FittedGrammars
 
     Returns:
-        None
+        modelToTaskToLogposterior (dict): dictionary with model nameas as keys and dictionaries of (task, logPosterior) entries as values
     """
 
     modelToLogPosteriors = {modelName:[] for modelName in modelNames}
@@ -78,73 +86,15 @@ def enumerationProxy(task2FittedGrammars, tasks, modelNames, verbose=False):
 
     return {modelName: {t: logPosterior for t,logPosterior in zip(tasksWithGtPrograms, logPosteriors)} for modelName,logPosteriors in modelToLogPosteriors.items()}
 
-# def enumerationProxy(task2FittedGrammar, train, grammar, nSim, task2groundTruthPrograms=None, neuralBaselinePath=NEURAL_RECOGNITION_MODEL_PATH, verbose=False):
-#     """
-#     Given a frontier of tasks prints out the logposterior of tasks in train using:
-#         - the RNN-encoded neural recogntion model
-#         - the unigram grammar fitted on nSim most similar tasks
-#     """
+def cumulativeNumberOfTasksSolved(enumerationFilename, enumerationTime):
+    frontiers, times = loadEnumerationResults(enumerationFilename)    
+    satisfiesHoldout = lambda f: len(f.entries) > 0 and f.task.check(f.topK(1).entries[0].program, timeout=1.0, leaveHoldout=False)
+    solvedTasks = set(f.task for f in frontiers if satisfiesHoldout(f))
+    approximateMAPSortedTimes = sorted([time for task,time in times.items() if task in solvedTasks])
 
-#     if task2groundTruthPrograms is not None:
-#         raise NotImplementedError
-
-#     recognitionModel = dill.load(open(NEURAL_RECOGNITION_MODEL_PATH, "rb"))
-
-#     uniformGrammarPriors, logVariableGrammarPriors, fittedLogPosteriors, neuralRecognitionLogPosteriors = 0.0, 0.0, 0.0, 0.0
-
-#     numTasks = 0
-#     fittedTasks = None
-#     if isinstance(task2FittedGrammar, list):
-#         fittedTasks = list(task2FittedGrammar[0].keys())
-#     else:
-#         fittedTasks = list(task2FittedGrammar.keys())
-#     for task in train:
-#         # if we have a fitted grammar for this task and a ground truth program we can score the grammar on
-#         if task in fittedTasks and task.program is not None:
-#             numTasks += 1
-#             vprint("\n-------------------------------------------------------------------------------", verbose)
-#             vprint(task.describe(), verbose)
-#             vprint("Ground Truth Program: {}".format(task.program), verbose)
-#             vprint("---------------------------------------------------------------------------------", verbose)
-#             uniformGrammarPrior = grammar.logLikelihood(task.request, task.program)
-#             vprint("Uniform Grammar Prior: {}".format(uniformGrammarPrior), verbose)
-#             logVariableGrammar = Grammar(2.0, [(0.0, p.infer(), p) for p in grammar.primitives], continuationType=None)
-#             logVariableGrammarPrior = logVariableGrammar.logLikelihood(task.request, task.program)
-#             vprint("Log Variable Program Prior: {}".format(logVariableGrammarPrior), verbose)
-#             vprint("---------------------------------------------------------------------------------", verbose)
-
-#             if isinstance(task2FittedGrammar, list):
-#                 toPrint = "PropSim Grammar LP ({} frontiers):".format(nSim)
-#                 for task2Grammar in task2FittedGrammar:
-#                     if task in task2Grammar:     
-#                         fittedLogPosterior = task2Grammar[task].logLikelihood(task.request, task.program)
-#                         toPrint += " {} ->".format(fittedLogPosterior)
-#                     else:
-#                         toPrint += "solved"
-#                 vprint(toPrint, verbose)
-#             else:
-#                 if task in task2FittedGrammar:
-#                     fittedLogPosterior = task2FittedGrammar[task].logLikelihood(task.request, task.program)
-#                     vprint("ProSim Grammar LP ({} frontier): {}".format(nSim, fittedLogPosterior), verbose) 
-            
-#             neuralGrammar = recognitionModel.grammarOfTask(task)
-#             neuralRecognitionLogPosterior = neuralGrammar.logLikelihood(task.request, task.program).item()
-#             vprint("Neural Recognition LogPosterior: {}\n".format(neuralRecognitionLogPosterior), verbose)
-            
-#             uniformGrammarPriors += uniformGrammarPrior
-#             logVariableGrammarPriors += logVariableGrammarPrior
-#             neuralRecognitionLogPosteriors += neuralRecognitionLogPosterior
-#             fittedLogPosteriors += fittedLogPosterior
-
-#     if numTasks == 0:
-#         print("No solved frontiers from which to report metrics")
-#         return
-#     else:
-#         print("Metrics for {} tasks with solutions".format(numTasks))
-
-#     print("Mean Uniform Grammar Prior: {}".format(uniformGrammarPriors / numTasks))
-#     print("Mean Log Variable Grammar Prior: {}".format(logVariableGrammarPriors / numTasks))
-#     print("Neural Recognition Log Posterior: {}".format(neuralRecognitionLogPosteriors / numTasks))
-#     print("Mean Fitted Log Posterior ({} frontiers): {}".format(nSim, fittedLogPosteriors / numTasks))
-
-#     return task2FittedGrammar
+    counts = 0
+    for time in approximateMAPSortedTimes:
+        if time > enumerationTime:
+            return counts
+        counts += 1
+    return counts
--- dreamcoder/domains/list/utilsPlotting.py
+++ dreamcoder/domains/list/utilsPlotting.py
@@ -1,34 +1,50 @@
 import dill
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 from dreamcoder.domains.list.compareProperties import compare
+from dreamcoder.domains.list.utilsEval import cumulativeNumberOfTasksSolved, loadEnumerationResults
 
 DATA_DIR = "data/prop_sig/"
 ENUMERATION_RESULTS_DIR = "enumerationResults/"
 SAMPLED_PROPERTIES_DIR = "sampled_properties/"
+ENUMERATION_TIME = 600
 
-def plotFrontiers(modelNames, fileNames=None, enumerationResults=None, save=True, plotName="enumerationTimes"):
+def plotFrontiers(modelNames, filenames=None, times=False, enumerationResults=None, save=True, plotName="enumerationTimes"):
 
     if enumerationResults is None:
-        if fileNames is None:
+        if filenames is None:
             assert Exception("You must either provide the filenames of the pickled enumeration results or the results themselves")
         else:
-            enumerationResults = [(frontiers, times) for fileName in dill.load(open(ENUMERATION_RESULTS_DIR + fileName, "rb"))]    
+            enumerationResults = [loadEnumerationResults(filename) for filename in filenames]
 
     plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.jet(np.linspace(0,1, len(modelNames))))
 
     for enumerationResult, modelName in zip(enumerationResults, modelNames):
         
         frontiers, times = enumerationResult
-        satisfiesHoldout = lambda f: f.task.check(f.topK(1).entries[0].program, timeout=1.0, leaveHoldout=False)
+        numTasks = len(frontiers)
 
+        satisfiesHoldout = lambda f: f.task.check(f.topK(1).entries[0].program, timeout=1.0, leaveHoldout=False)
         nonEmptyFrontiers = [f for f in frontiers if len(f.entries) > 0]
         logPosteriors = sorted([-f.bestPosterior.logPosterior for f in nonEmptyFrontiers if satisfiesHoldout(f)])
         print("{}: {} / {}".format(modelName, len(logPosteriors), len(nonEmptyFrontiers)))
-        plt.plot(logPosteriors, [i / len(frontiers) for i in range(len(logPosteriors))], label=modelName, alpha=0.6)
+
+        satisfiesHoldout = lambda f: len(f.entries) > 0 and f.task.check(f.topK(1).entries[0].program, timeout=1.0, leaveHoldout=False)
+        solvedTasks = set(f.task for f in frontiers if satisfiesHoldout(f))
+        if times:
+            toPlot = sorted([time for task,time in times.items() if task in solvedTasks])
+            percentTasksSolved = [i / numTasks for i in range(len(toPlot))]
+            # adding point to the end so that plot lines extend all the way to the far right side of the plot
+            plt.plot(toPlot + [ENUMERATION_TIME], percentTasksSolved + [percentTasksSolved[-1]], label=modelName, alpha=0.6)
+        else:
+            toPlot = sorted([-f.bestPosterior.logPosterior for f in frontiers if f.task in solvedTasks])
+            plt.plot(toPlot, [i / numTasks for i in range(len(toPlot))], label=modelName, alpha=0.6)
 
     plt.ylim(bottom=0, top=1)
+    plt.xlabel("Enumeration time (s)")
+    plt.ylabel("Percent tasks solved")
     plt.legend()
     plt.show()
     if save:
@@ -80,6 +96,92 @@ def plotNumSampledPropertiesVersusTimeout(timeouts, propertiesFilenames, handwri
     plt.xlabel("Property Enumeration Time (s)")
     plt.show()
     return
+
+def barPlotPerTaskStatistics(modelNames, enumerationFilenames, grammarFilenames=None, plotTime=True, includeUnsolved=False):
+    if includeUnsolved:
+        if grammarFilenames is None:
+            raise Exception("You must provided the fitted grammars if you want to include the unsolved tasks in the plot")
+        else:
+            grammars = [loadEnumerationResults(filename) for filename in grammarFilenames]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.jet(np.linspace(0,1, len(modelNames))))
+
+    frontiers, times = loadEnumerationResults(enumerationFilenames[0])
+
+    if plotTime:
+        toPlot = [(task,time) if time is not None else (task, -10) for task,time in times.items()]
+    else:
+        toPlot = [(f.task, -f.topK(1).entries[0].logPosterior) if len(f.entries) > 0 is not None 
+        else (f.task, (-grammars[0][f.task].logLikelihood(f.task.request, f.task.program) if includeUnsolved else 0)) for f in frontiers]
+    
+    sortedTimes = sorted(toPlot, key=lambda x: x[1])
+    tasks, toPlot = list(zip(*sortedTimes))
+    taskNames = [t.name for t in tasks]
+
+    # Calculate optimal width
+    width = (1.0 / float(len(enumerationFilenames))) / 2.0
+    ax.bar([x_coord for x_coord in range(len(tasks))], toPlot, width, label=modelNames[0], align="edge")
+
+    offset = width
+    for i in range(1, len(modelNames)):
+        modelName, enumerationFilename = modelNames[i], enumerationFilenames[i]
+        frontiers, times = loadEnumerationResults(enumerationFilename)
+        task2frontier = {f.task:f for f in frontiers}
+
+        if plotTime:
+            toPlot = [times[task] if times[task] is not None else -10 for task in tasks]
+        else:
+            toPlot = [-task2frontier[task].topK(1).entries[0].logPosterior if len(task2frontier[task].entries) > 0 is not None 
+            else (-grammars[i][task].logLikelihood(task.request, task.program) if includeUnsolved else 0) for task in tasks]
+
+        ax.bar([x_coord + offset for x_coord in range(len(tasks))], toPlot, width, label=modelName, align="edge")
+        offset += width
+
+    ax.axes.set_xticklabels(taskNames)
+    ax.set_ylabel('Negative log probability')
+    ax.set_xlabel('Concepts')
+    plt.xticks(rotation=90, fontsize=6)
+    plt.legend()
+    plt.show()
+
+    return
+
+def main():
+    modelNames, enumerationFilenames, grammarFilenames = zip(*[
+        ############ Random primitive weights (seed=1) #############
+        # "propsim-fitted": "propsimGrammarsHandwritten_2022-01-01_23:19:30.389021_t=600.pkl",
+        # # "propsimGrammarsHandwrittenEqWeight": "propsimGrammarsHandwrittenEqWeight_2022-01-01_23:36:17.413812_t=600.pkl",
+        # # "propsimGrammarsAutomatic_2022": "propsimGrammarsAutomatic_2022-01-02_00:10:34.733461_t=600.pkl",
+        # # "propsimGrammarsAutomaticEqWeight_2022-01-01_23:53:34.640085_t=600.pkl",
+        # "all-fitted": "helmholtzFitted_2022-01-02_00:20:35.402688_t=600.pkl",
+
+        ############ Equal weight primitives #############
+        ("PropsimFit", "propsimGrammarsHandwritten_2022-01-01_22:14:36.614585_t=600.pkl", "data/prop_sig/helmholtz_frontiers/josh_rich_0_10_enumerated/13742_with_josh_fleet_0_10-inputs__propSim_propToUse=handwritten_numHelmFrontiers_10000_nSim=50_weightedSim=False_onlyTrueProp=_False_taskSpecificInputs=True_compressSimilar=False_equalWprop=False_seed=2_grammars.pkl"), 
+        # "propsimGrammarsHandwrittenEqWeight_2022-01-01_22:30:59.723239_t=600.pkl",
+        # "propsimGrammarsAutomaticEqWeight_2022-01-01_22:47:51.005722_t=600.pkl",
+        # "propsimGrammarsAutomatic_2022-01-01_23:04:27.669035_t=600.pkl",
+        ("AllFit", "helmholtzFitted_2022-01-04_13:45:58.645759_t=600.pkl", "data/prop_sig/helmholtz_frontiers/josh_rich_0_10_enumerated/13742_with_josh_fleet_0_10-inputs__helmholtz_10000_grammars.pkl"),
+        ("Uniform", "uniform_2022-01-04_14:07:30.300207_t=600.pkl", None)
+    ])
+
+    # grammarFilenames = [
+    #     "propsimGrammarsHandwritten_2022-01-03_22:02:28.760806_t=5.pkl",
+    #     "neuralGrammars_2022-01-03_22:02:40.586424_t=5.pkl",
+    #     "helmholtzGrammar_2022-01-03_22:02:45.937921_t=5.pkl"
+    # ]
+
+    for i in [10,25,50,100,600]:
+        print("-------------------------------------------------------")
+        for modelName,enumerationFilename in zip(modelNames, enumerationFilenames):
+            print("{}: {}".format(modelName, cumulativeNumberOfTasksSolved(enumerationFilename, i)))
+
+    plotFrontiers(modelNames, filenames=enumerationFilenames, times=True, enumerationResults=None, save=True, plotName="enumerationTimes")
+
+    # barPlotPerTaskStatistics(modelNames, enumerationFilenames, grammarFilenames, plotTime=True, includeUnsolved=False)
+    return 
+
 
 # handwrittenProperties = getHandwrittenPropertiesFromTemplates(tasks)
 # valuesToInt = {"allFalse":0, "allTrue":1, "mixed":2}
